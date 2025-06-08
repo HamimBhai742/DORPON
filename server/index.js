5f42ef93ff8f0d20f507c877c0024c740c13bcf6
HamimBhai742 <omagootruelove147@gmail.com>
2025-06-08 19:47:45 +0600

    Register & Login api/ successfully complete

--- server/index.js
+++ server/index.js
@@ -8,12 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-// const authRoutes = require('./routes/auth');
-// app.use('/api/auth', authRoutes);
+
+//Routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+});
+
+app.get('/', (req, res) => {
+  res.send(`Server running on port ${process.env.PORT}`);
 });
